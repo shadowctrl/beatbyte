17b1df09866bdd1f6b7e09754c3e4865b247bc2f
Shadowctrl <shadowctrl@outlook.com>
2025-02-03 17:01:03 +0530

    Add Banner component with styling and layout

---
+++ src/components/Banner/banner.tsx
@@ -0,0 +1,34 @@
+import { NextPage } from "next";
+import "./banner.css";
+import Image from "next/image";
+import { bannerDataType } from "@/types";
+
+interface Props {
+  data: bannerDataType;
+}
+
+const Banner: NextPage<Props> = ({
+  data: { imgUrl, title, smallText, bigText, buttonText, description },
+}) => {
+  return (
+    <div>
+      <div className="bannerMain">
+        <div className="bannerText">
+          <h3>{title}</h3>
+          <h4>{smallText}</h4>
+          <h5>{bigText}</h5>
+          <button>{buttonText}</button>
+        </div>
+        <div className="bannerImage">
+          <Image src={imgUrl} width={450} height={450} alt="Banner Image" />{" "}
+        </div>
+        <div className="desc">
+          <h3>Description</h3>
+          <p>{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
