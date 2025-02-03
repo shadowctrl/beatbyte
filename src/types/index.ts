1e67951d374a06e8d2b6bebdfece79d08cfeb182
Shadowctrl <shadowctrl@outlook.com>
2025-02-03 17:02:01 +0530

    Add bannerDataType interface to define banner data structure

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,12 @@ export interface navItemsType {
   name: string;
   path: string;
 }
+
+export interface bannerDataType {
+  imgUrl: string;
+  title: string;
+  smallText: string;
+  bigText: string;
+  buttonText: string;
+  description: string;
+}
