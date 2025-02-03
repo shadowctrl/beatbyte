1bd7b1e305142cabd278167a85c8f7c2bc469115
Shadowctrl <shadowctrl@outlook.com>
2025-02-03 15:13:33 +0530

    Constants - NavItems

---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+export const navItems = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Products",
+    path: "/products",
+  },
+  {
+    name: "About Us",
+    path: "about-us",
+  },
+];
