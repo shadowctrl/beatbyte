4db7ce159176e18350215af23b00a8b804dea63d
Shadowctrl <shadowctrl@outlook.com>
2025-02-04 12:40:32 +0530

    Add ClerkLoading component to display loading state in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ClerkLoaded, ClerkProvider } from "@clerk/nextjs";
+import { ClerkLoaded, ClerkLoading, ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +29,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <ClerkLoading>Clerk is Loading</ClerkLoading>
           <ClerkLoaded> {children}</ClerkLoaded>
         </body>
       </html>
